a4bbfc63f4d09a18f407f2eb369641697f0ec88e
Unisogdi <younes.sogandi@edu.dsti.institute>
2025-12-19 08:22:01 +0100

    Delete userapi/tests/test_app.py

--- userapi/tests/test_app.py
+++
@@ -1,6 +0,0 @@
-from src.app import app   # src correspond au dossier où se trouve ton app.py
-
-def test_health():
-    client = app.test_client()
-    response = client.get("/health")
-    assert response.status_code == 200
